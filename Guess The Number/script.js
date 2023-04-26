fbc66a7362464a36743013f687410966930d7bb0
Upendra Kumar <toxic74412@gmail.com>
2023-04-26 20:18:16 +0530

    completed front-end for guess the numbers

---
+++ Guess The Number/script.js
@@ -0,0 +1,20 @@
+const alertBox = document.querySelector(".alertbox");
+const button = document.querySelector(".check");
+const input = document.querySelector(".guess");
+
+const generateRandomNumber = () => {};
+
+/*
+
+
+const showAlert = () => {
+  alertBox.style.right = "3%";
+};
+
+const hideAlert = () => {
+  alertBox.style.right = "-100%";
+};
+
+// setTimeout(showAlert, 3000);
+// setTimeout(hideAlert, 10000);
+*/
