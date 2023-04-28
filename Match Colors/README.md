2f0277c4ad1e71160dce2c20bc903ba4d63ded82
Upendra Kumar <toxic74412@gmail.com>
2023-04-28 23:47:25 +0530

    About to complete

--- Match Colors/README.md
+++ Match Colors/README.md
@@ -1,7 +1,1 @@
-Problem
-
-So problem ye hai ki mere pass 2 ball hai aur dono ko maine boolean value assign ki hai jo ki hai false.
-Ab mai chahta hu ki jab mai ek ko ball ko touch karu to uska value true ho jaye aur dusre ka false but jab mai dusre ko touch karu to uska value true ho jaye aur dusre ka false.
-
-any solution ?
-You can also change the predefined values.
+# Near to Complete
--- Match Colors/script.js
+++ Match Colors/script.js
@@ -3,6 +3,18 @@ const instructionIcon = document.querySelector(".icons");
 const alert = document.querySelector(".alertbox");
 const leftBox = document.querySelector(".left__box");
 const rightBox = document.querySelector(".right__box");
+const leftRedColorValue = document.querySelector(".leftRed");
+const leftGreenColorValue = document.querySelector(".leftGreen");
+const leftBlueColorValue = document.querySelector(".leftBlue");
+const rightRedColorValue = document.querySelector(".rightRed");
+const rightGreenColorValue = document.querySelector(".rightGreen");
+const rightBlueColorValue = document.querySelector(".rightBlue");
+const redIncrease = document.querySelector(".redIncrease");
+const redDecrease = document.querySelector(".redDecrease");
+const greenIncrease = document.querySelector(".greenIncrease");
+const greenDncrease = document.querySelector(".greenDecrease");
+const blueIncrease = document.querySelector(".blueIncrease");
+const blueDncrease = document.querySelector(".blueDecrease");
 
 const toggleInstruction = () => {
   if (instructionBox.style.right === "-100%") {
@@ -21,7 +33,6 @@ const callToggleBoxes = () => {
     showAlert("LEFT IS SELECTED🔥!", "3%");
     setTimeout(hideAlert, 4000);
   });
-
   rightBox.addEventListener("click", () => {
     rightBox.setAttribute("aria-selected", "true");
     leftBox.setAttribute("aria-selected", "false");
@@ -39,4 +50,54 @@ const showAlert = (message, styleProp) => {
 
 const hideAlert = () => {
   alert.style.right = "-100%";
+};
+
+const generateRGB = () => {
+  const red = Math.floor(Math.random() * 255 + 1);
+  const green = Math.floor(Math.random() * 255 + 1);
+  const blue = Math.floor(Math.random() * 255 + 1);
+  return { red: red, green: green, blue: blue };
+};
+
+const setBgColor = () => {
+  const leftRGB = generateRGB();
+  const rightRGB = generateRGB();
+  leftBox.style.backgroundColor = `rgb(${leftRGB.red}, ${leftRGB.green}, ${leftRGB.blue})`;
+  rightBox.style.backgroundColor = `rgb(${rightRGB.red}, ${rightRGB.green}, ${rightRGB.blue})`;
+
+  leftRedColorValue.value = leftRGB.red;
+  leftGreenColorValue.value = leftRGB.green;
+  leftBlueColorValue.value = leftRGB.blue;
+  rightRedColorValue.value = rightRGB.red;
+  rightGreenColorValue.value = rightRGB.green;
+  rightBlueColorValue.value = rightRGB.blue;
+};
+
+setBgColor();
+
+const increaseDecreaseValue = (r, g, b) => {
+  redIncrease.addEventListener("click", () => {
+    let redValue = parseInt(r.value);
+    r.value = redValue + 1;
+  });
+  redDecrease.addEventListener("click", () => {
+    let redValue = parseInt(r.value);
+    r.value = redValue - 1;
+  });
+  greenIncrease.addEventListener("click", () => {
+    let greenValue = parseInt(g.value);
+    g.value = greenValue + 1;
+  });
+  greenDncrease.addEventListener("click", () => {
+    let greenValue = parseInt(g.value);
+    g.value = greenValue - 1;
+  });
+  blueIncrease.addEventListener("click", () => {
+    let blueValue = parseInt(b.value);
+    b.value = blueValue + 1;
+  });
+  blueDncrease.addEventListener("click", () => {
+    let blueValue = parseInt(b.value);
+    b.value = blueValue - 1;
+  });
 };
