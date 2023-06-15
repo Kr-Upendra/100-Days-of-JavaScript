d631f4baab4d0c68f128727b12ca4e00c35279b6
Upendra Kumar <toxic74412@gmail.com>
2023-06-15 13:15:42 +0530

    Completed 2nd section of recipe project

--- Recipes/script.js
+++ Recipes/script.js
@@ -1,5 +1,7 @@
 const menuIcon = document.querySelector(".navbar__bars");
 const navigation = document.querySelector(".navbar__links");
+const prevBtn = document.querySelector(".prev__btn");
+const nextBtn = document.querySelector(".next__btn");
 
 menuIcon.addEventListener("click", () => {
   const attValue = menuIcon.getAttribute("data-menu");
@@ -11,3 +13,23 @@ menuIcon.addEventListener("click", () => {
     navigation.style.top = "-100%";
   }
 });
+
+let slideIndex = 1;
+
+function slideRecipes(n) {
+  let i;
+  let slides = document.getElementsByClassName("card");
+  if (n > slides.length) slideIndex = 1;
+  if (n < 1) slideIndex = slides.length;
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+slideRecipes(slideIndex);
+
+function plusSlide(n) {
+  console.log("Hello");
+  slideRecipes((slideIndex += n));
+}
